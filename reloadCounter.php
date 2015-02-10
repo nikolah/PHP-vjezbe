5e36bc0602374925bac3db29fc1dd70dd168fef2
nikolah <n.d.hrcic@gmail.com>
2015-02-10 12:04:43 +0000

    Small code with $_SESSIONs counts reloads of the site

---
+++ reloadCounter.php
@@ -0,0 +1,20 @@
+﻿<?php
+		// brojač reloadinga
+	session_start();
+
+	if (!isset($_SESSION['count4'])) {  // da nebi kod prvog loadanja javljalo grešku
+		$_SESSION['count4'] = 0;  
+		} 
+	else {
+		$_SESSION['count4']++ ;  
+		}
+
+	echo $_SESSION['count4'] ;
+		
+		// "s povratom nakon 5"
+	/*
+	if ($_SESSION['count4'] >= 5 || $_SESSION['count4'] == 0){
+	$_SESSION['count4'] = 1;
+	*/
+	
+?>
