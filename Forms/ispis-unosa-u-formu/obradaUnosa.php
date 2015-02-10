9375159816c529d0dfc4630314d36edced5298a2
nikolah <n.d.hrcic@gmail.com>
2015-02-10 14:49:47 +0000

    Processing file

---
+++ Forms/ispis-unosa-u-formu/obradaUnosa.php
@@ -0,0 +1,16 @@
+﻿<?php
+	session_start();
+
+	if(isset($_POST['rijec'])) {
+		if ($_POST['rijec'] == null || ($_POST['rijec']) == ''){
+			$izjava = 'Nema unosa';
+			} else {
+			$izjava = $_POST['rijec'];
+			}
+		$_SESSION['izjavaa'] = $izjava;
+	}
+
+	header('Location: formaIspis.php');
+?>
+		
+		
