862bc9837e9453b14d8c191f9400975d2c60444e
nikolah <n.d.hrcic@gmail.com>
2015-02-12 11:45:11 +0000

    Click counter

---
+++ Forms/brojac-klikova/brojacClick2.php
@@ -0,0 +1,13 @@
+﻿<?php 
+echo '<br>';
+echo '<span style="color:c0c0c0; font-size:19px;">Click counter</span><br><br>';
+session_start()?>
+<form method="post" action="brojacClick2_obrada.php">
+<input type="submit" name="next" value="Next"/>
+<input type="submit" name="clear" value="Clear"/>
+</form>
+
+<?php 
+	$ispisClick = $_SESSION['cnt'];
+	echo $ispisClick;
+?>
