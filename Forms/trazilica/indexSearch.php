c4dcec794ddbed55400888c33646f2088deb6def
nikolah <n.d.hrcic@gmail.com>
2015-02-12 13:42:15 +0000

    Simple search engine

---
+++ Forms/trazilica/indexSearch.php
@@ -0,0 +1,25 @@
+﻿<meta charset="UTF-8">
+<br>
+<span style="font-size:22; color:c0c0c0;">Pravimo primitivnu tražilicu (Search engine)</span>
+<?php	
+	session_start();
+?>
+<br><br>
+	<table>
+		<tr>
+			<td>Upiši riječ:</td>
+			<form method="POST" action="obradaSearch.php">
+			<td><input type="text" name="rijec1"></input></td>
+			<td><input type="submit" value="   Potvrdi   "></input></td>
+			</form>
+		</tr>
+		<tr>
+			<td>Upiši slovo ili riječ pretrage:</td>
+			<form method="POST" action="obradaSearch.php">
+			<td><input type="text" name="rijec2"></input></td>
+			<td><input type="submit" value="   Potvrdi   "></input></td>
+			</form>
+		</tr>
+	</table>
+<br>
+
