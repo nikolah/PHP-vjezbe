7ce363f1470752e7503e6c90923f47256d12d364
nikolah <n.d.hrcic@gmail.com>
2015-02-12 11:51:09 +0000

    Processing file

---
+++ Forms/brojac-klikova/brojacClick2_obrada.php
@@ -0,0 +1,12 @@
+﻿<?php
+   session_start();
+
+   if(isset($_POST['next'])){
+       $_SESSION['cnt']++;
+     } 
+   if(isset($_POST['clear'])) {
+	   $_SESSION['cnt'] = 0;
+     }
+	
+	header('Location:brojacClick2.php');
+?>
