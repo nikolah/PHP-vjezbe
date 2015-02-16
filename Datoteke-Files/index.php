2db2e44f95c62734056469775485e3c553d0a544
nikolah <n.d.hrcic@gmail.com>
2015-02-16 10:24:56 +0000

    Simple exercises using files

---
+++ Datoteke-Files/index.php
@@ -0,0 +1,93 @@
+﻿<meta charset="UTF-8">
+<br>
+<span style="font-size:24; color:c0c0c0;">Upisani podaci upisuju se u datoteku, uz moguću pretragu i ispis</span>
+<br><br><br>
+	<table>
+	<tr>
+		<td>Upiši riječ:</td>
+		<form method="POST" action="obradaIndexa4.php">
+		<td><input type="text" name="rijec1"></td>
+		<td><input type="submit" value="   Potvrdi    "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">
+		<td>Upiši slovo ili riječ pretrage:</td>
+		<td><input type="text" name = "rijec2"></td>
+		<td><input type="submit" value="   Potvrdi    "></td>
+		</form>
+	</tr>
+	</table>
+<br>
+	<table>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='brojZnakova' value="  Broj upisanih slova  "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='brojRedova' value="  Broj redova u datoteci  "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='brojRijeci' value="  Broj upisanih riječi  "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='brojRcenica' value="  Broj upisanih rečenica  "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='zadnjaRijec' value="  Posljednje upisana riječ/riječi  "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='sveRijeci' value="  Sve upisane riječi  "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='rijecPretrage' value="  Riječ pretrage  "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='rezultatPretrage' value="  Rezultat pretrage  "></td>
+		</form>
+	</tr>	
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='deletePretraga' value="  Izbriši riječ pretrage  "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='niz' value="  Ispiši sve riječi datoteke kao niz  "></td>
+		</form>
+	</tr>
+	</table>
+<br>
+
+	<table>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='sve' value="  Ispiši sve   "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='sveRijeci' value="  Obriši sve  "></td>
+		</form>
+	</tr>
+	<tr>
+		<form method="POST" action="obradaIndexa4.php">	
+		<td><input type="submit" name='brisanje' value="  Obriši sve podatke u datoteci  "></td>
+		</form>
+	</tr>
+
+	</table>
