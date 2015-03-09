41ffadc6fb020e3d87add96e9cfbe856606312d4
nikolah <n.d.hrcic@gmail.com>
2015-03-09 21:01:13 +0000

    Processing file

---
+++ Forms/bodyMass/bmi-process.php
@@ -0,0 +1,14 @@
+﻿<?php
+	session_start();
+	$bmi = 'ništa';
+	if(isset($_POST['weight']) && isset($_POST['height']) && is_numeric($_POST['weight']) && is_numeric($_POST['height']))  {
+		$weight = $_POST['weight'];
+		$height = $_POST['height'];
+		$bmi = 'Your BMI: ' . $weight / pow($height,2);
+	} else {
+		$bmi = ''; // 'Wrong enter'
+	}
+	$_SESSION['bmi'] = $bmi;
+	
+	header('Location: bmi.php');
+?>
